1eb499f0fe11609ee9c2510a4d0c127380618880
Thiago <thiagofalkiev@hotmail.com.ar>
2023-04-24 17:33:50 -0300

    Cambios nombres y FrontEnd

---
+++ Tarea 2.1 - FrontEnd - Login Banco Santander/Main/app.js
@@ -0,0 +1,9 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+    } else {
+      passwordInput.type = 'password';
+    }
+  }
+  
